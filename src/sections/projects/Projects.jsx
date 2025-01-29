53d262347bc20e33abcdd0b9f9975aa83a5f5ed0
Patrik Malta <maltapatrik3@gmail.com>
2025-01-29 19:11:32 -0400

    tirei o miaw facts

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -6,14 +6,6 @@ const Projects = () => {
       <h2>Projetos</h2>
       <div className="project-grid">
         <div className="project">
-          <h3>Miaw Facts</h3>
-          <p>Projeto que fiz para estudar consumo de API. Se trata de um site onde o usuario pode descobrir 
-             curiosidades do mundo felino. <br/>Feito com ReactJs e API externa. Obs (Feito em 2024)</p>
-             <div className="link-container">
-             <a href="https://patrikmaltacm.github.io/miaw-facts/">Veja o projeto</a>
-             </div>
-        </div>
-        <div className="project">
           <h3>Pixel Home</h3>
           <p>Estou fazendo ele para melhorar
             todas as habilidades que tenho em fullstack, fiz a API de coleta de imagens enviadas
