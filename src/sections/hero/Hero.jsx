6f07b43929dd0772bb8058981f714dbc87aaf185
Patrik Malta <maltapatrik3@gmail.com>
2025-01-28 22:25:39 -0400

    pronto versao 0.2

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
@@ -7,9 +6,11 @@ const Hero = () => {
       <div className="hero-content">
         <h1>Patrik Malta</h1>
         <p>Desenvolvedor Fullstack | React | Node.js</p>
-        <button>
-          <Link to="about" smooth={true} duration={500}>Me conheça melhor</Link>
-        </button>
+        <a href="https://github.com/PatrikMaltacm" target='blank'>
+        <img src="https://img.icons8.com/?size=100&id=62856&format=png&color=000000" alt="github-logo"  width={80}/>
+        </a><a href="https://linkedin.com/in/patrik-malta-1160552b6" target='blank'>
+        <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="linkedin-logo" width={80}/>
+        </a>
       </div>
     </section>
   );
